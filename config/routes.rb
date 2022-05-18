7da40abed7d65dbd9d3c34c32ab5a14815cb7d78
Sy Rashid <sy.rashid.11@gmail.com>
2022-05-18 10:06:24 +0100

    FEAT: Add collection and member routes to app

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :restaurants do
+    collection do
+      get :top
+      get :worst
+    end
+    member do
+      get :chef
+    end
+  end
+  # get 'restaurants/top', to: 'restaurants#top'
+  # get 'restaurants/:id/chef', to: 'restaurants#chef'
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts "Cleaning DB"
+Restaurant.destroy_all
+
+puts "Creating Restaurants..."
+50.times do
+  restaurant =  Restaurant.create(name: Faker::Restaurant.name, address: Faker::Address.street_address, stars: rand(1..5))
+  puts "#{restaurant.name} created"
+end
+
+puts "#{Restaurant.count} restaurants created!"
