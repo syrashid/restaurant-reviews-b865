b885be27fc68be225e39de419f2982001772e5ed
Sy Rashid <sy.rashid.11@gmail.com>
2022-05-18 11:03:18 +0100

    FEAT: Finish review portion of lecture

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,9 @@ Rails.application.routes.draw do
     member do
       get :chef
     end
+    resources :reviews, only: [:new, :create]
   end
+  resources :reviews, only: :destroy
   # get 'restaurants/top', to: 'restaurants#top'
   # get 'restaurants/:id/chef', to: 'restaurants#chef'
 end
